e084bec4286751e15d7f69881e8062e3c38be95f
Samuel Blake <samuelblake97@gmail.com>
2019-05-08 14:36:52 -0700

    added my stuff

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@
 exports.view = function(req, res){
   res.render('index', {
     'projects': [
-      { 'name': 'Waiting in Line',
-        'image': 'lorempixel.people.1.jpeg',
+      { 'name': 'TEA',
+        'image': 'TEA.png',
         'id': 'project1'
       },
       { 'name': 'Needfinding',
